cfc273b916c6e5ffe7989097b3dce7816d36cb64
Kalmar Lorand <lorand.kalmar@wolterskluwer.com>
2025-01-07 15:18:54 +0200

    modify progress bar

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -1,28 +1,38 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as ProgressPrimitive from "@radix-ui/react-progress"
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import { cn } from "@/lib/utils";
 
-import { cn } from "@/lib/utils"
+interface ProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  progressBarColor: string;
+}
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({ className, value, ...props }, ref) => (
+  ProgressProps
+>(({ className, value, progressBarColor, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-2 w-full overflow-hidden rounded-full bg-primary/20",
+      "relative h-8 w-full overflow-hidden rounded-md bg-beige-100 p-1",
       className
     )}
     {...props}
   >
-    <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
+    <div className="h-full w-full overflow-hidden rounded-sm">
+      <ProgressPrimitive.Indicator
+        className="h-full w-full flex-1 transition-all rounded-sm"
+        style={{
+          transform: `translateX(-${100 - (value || 0)}%)`,
+          backgroundColor: progressBarColor,
+        }}
+      />
+    </div>
   </ProgressPrimitive.Root>
-))
-Progress.displayName = ProgressPrimitive.Root.displayName
+));
 
-export { Progress }
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
