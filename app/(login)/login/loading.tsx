519d2ebce207f5170383122defc23b0bd5ed6700
Kalmar Lorand <lorand.kalmar@wolterskluwer.com>
2025-01-26 23:13:15 +0200

    add loading messages

---
+++ app/(login)/login/loading.tsx
@@ -0,0 +1,12 @@
+import Spinner from "@/components/ui/spinner";
+
+export default function Loading() {
+  return (
+    <div className="grid items-center justify-center">
+      <Spinner />
+      <p className="text-preset-1 text-muted-foreground">
+        Logging in...
+      </p>
+    </div>
+  );
+}
